ba16a8005b5780a56bedbbab00183349de118ab5
Nargiza Naamatbekova <naamatbeknargiza@gmail.com>
2025-03-15 23:45:36 +0600

    create-inputs

---
+++ src/components/UI/InputField.jsx
@@ -0,0 +1,56 @@
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+
+export default function InputField() {
+   return (
+      <Box
+         sx={{
+            border: '2px dashed #aaa',
+            maxWidth: '400px',
+            padding: '16px',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+
+            '& .MuiTextField-root': {
+               width: '35ch',
+            },
+         }}
+      >
+         <TextField
+            label={'Название праздника'}
+            id="margin-dense"
+            margin="dense"
+            placeholder="Введите название праздника"
+         />
+         <TextField
+            label={'Название праздника'}
+            id="margin-dense"
+            margin="dense"
+            placeholder="Введите название праздника"
+         />
+         <TextField
+            label={'Название праздника'}
+            id="margin-dense"
+            margin="dense"
+            placeholder="Введите название праздника"
+         />
+         <TextField
+            label={'Название праздника'}
+            id="margin-dense"
+            margin="dense"
+            placeholder="Введите название праздника"
+         />
+         <TextField
+            label={'Название праздника'}
+            id="margin-dense"
+            margin="dense"
+            fullWidth
+            placeholder="345787654356"
+            error //
+            helperText="Текст ошибки"
+         />
+      </Box>
+   )
+}
