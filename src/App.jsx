ec8bb7ad005d514539b3249a84e6aa4149dbee1c
Bekmyrza200800 <bekmyrza986@gmail.com>
2025-03-25 14:00:28 +0600

    Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-const App = () => <h1>Gift List JS-16</h1>
+import OpenModal from "./components/UI/Modal"
+
+const App = () => (
+   <h1>
+      <OpenModal />
+      Gift List JS-16
+   </h1>
+)
 
 export default App
