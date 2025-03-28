8ecaaf2445724ab7ebf5a26e245d9fc1db57e2f7
Bekmyrza200800 <bekmyrza986@gmail.com>
2025-03-28 14:24:23 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-const App = () => <h1>Gift List JS-16</h1>
+import InputSearch from './components/UI/Input-search/InputSearch'
+
+const App = () => (
+   <h1>
+      <InputSearch />
+      Gift List JS-16
+   </h1>
+)
 
 export default App
---
+++ src/utils/constants/index.js
@@ -0,0 +1,34 @@
+const conditions = [
+   { value: '1', name: 'Все' },
+   { value: '2', name: 'Б/У' },
+   { value: '3', name: 'Новое' },
+]
+
+const categories = [
+   { value: '1', name: 'Смартфоны и телефоны' },
+   { value: '2', name: 'Аудиотехника' },
+   { value: '2', name: 'Фото и видеокамеры' },
+   { value: '2', name: 'Автоэлектроника' },
+   { value: '2', name: 'ТВ и видео' },
+   { value: '2', name: 'Компьютеры, ноутбуки и планшеты' },
+]
+
+const subcategories = [
+   { value: '1', name: 'Электроника' },
+   { value: '2', name: 'Одежда' },
+   { value: '2', name: 'Школа' },
+   { value: '2', name: 'Дом и сад' },
+   { value: '2', name: 'Обувь' },
+   { value: '2', name: 'Транспорт' },
+]
+
+const countries = [
+   { value: '1', name: 'Кыргызстан' },
+   { value: '2', name: 'Азербайджан' },
+   { value: '2', name: 'Россия' },
+   { value: '2', name: 'Казахстан' },
+   { value: '2', name: 'Узбекистан' },
+   { value: '2', name: 'Таджикистан' },
+]
+
+export { conditions, categories, subcategories, countries }
