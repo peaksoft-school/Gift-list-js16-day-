a50cc327d0861912e64a9a62032363960498a9c5
Nurgul Madanbekova <nurgulya792@gmail.com>
2025-03-28 12:57:16 +0600

    clear linear

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -12,12 +12,6 @@ const theme = createTheme({
          main: '#8D949E',
          red: '#FD5200',
       },
-
-      linear: {
-         main: rgba(3, 78, 156, 1),
-         red: rgba(224, 70, 72, 0.938340301941089),
-         green: rgba(37, 187, 78, 0.8851190134256828),
-      },
    },
 })
 
