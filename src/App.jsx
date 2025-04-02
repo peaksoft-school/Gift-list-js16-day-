0060e011f46cee9ba71f4b99e6785a559ca40f09
Nargiza Naamatbekova <naamatbeknargiza@gmail.com>
2025-04-02 21:55:00 +0600

    save checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-const App = () => <p>Gift List-16</p>
+import { useState } from 'react'
+import Checkbox from './components/UI/Checkbox'
+const App = () => {
+   const [checked, setChecked] = useState(false)
 
+   const handleChange = (event) => {
+      setChecked(event.target.checked)
+   }
+
+   return (
+      <>
+         <Checkbox />
+      </>
+   )
+}
 export default App
