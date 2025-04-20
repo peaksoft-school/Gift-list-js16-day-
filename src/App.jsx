764566e319608eb9e67daa8e3e2c9b3716c2897c
Aigerim Mamattoichu <aigerimma993@gmail.com>
2025-04-20 09:53:56 -0700

    create-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './layout/Header'
+import SideBar from './components/UI/SideBar'
 
 const App = () => {
    return (
       <div>
-         <Header />
+         <Header role="user" />
+         <Header role="admin" />
       </div>
    )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,25 +1,73 @@
-import React from 'react'
 import MeetBalls from '../components/UI/MeetBalls'
-import styled from 'styled-components'
 import Notification from '../assets/icons/notification.svg'
-
 import InputSearch from '../components/UI/Input-search/InputSearch'
+import Input from '../components/UI/Input'
+import React from 'react'
+import { AppBar, Toolbar, Box, TextField, Typography } from '@mui/material'
 
-const Header = () => {
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search'
+
+const Header = ({ role }) => {
    return (
-      <StyledHeader>
-         <InputSearch />
-         <img src={Notification} alt="" style={{ marginRight: '10px' }} />
-         <MeetBalls />
-      </StyledHeader>
+      <AppBar
+         position="static"
+         sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}
+      >
+         <Toolbar
+            sx={{
+               flexDirection: 'column',
+               alignItems: 'flex-start',
+               gap: 1,
+            }}
+         >
+            <StyledBox>
+               {role === 'user' ? (
+                  <InputSearch sx={{ width: '821', height: '40px' }} />
+               ) : role === 'admin' ? (
+
+
+
+                  <StyledInput
+                     label="Поиск"
+                     variant="outlined"
+                     
+                  />
+               ) : null}
+
+               <img src={Notification} alt="" />
+
+               <MeetBalls />
+            </StyledBox>
+         </Toolbar>
+      </AppBar>
    )
 }
 
-export default Header
-
-const StyledHeader = styled('div')({
+const StyledBox = styled(Box)(() => ({
    display: 'flex',
-   justifyContent: 'space-around',
-   backgroundColor: 'rgb(245, 245, 247)',
-   padding: '20px 40px 20px 0px',
-})
+   alignItems: 'center',
+   justifyContent: 'space-between',
+   width: '1086px',
+   height: '40',
+   top: '23px',
+   left: '20px',
+   marginLeft: '10px',
+}))
+const StyledInput = styled(TextField)(() => ({
+   '& .MuiInputBase-root': {
+      width: '821px',
+      height: '40px',
+      boxSizing: 'border-box',
+      display: 'flex',
+      alignItems: 'center',
+   },
+   '& input': {
+      height: '100%',
+      padding: '20px ',
+      boxSizing: 'border-box',
+      alignItems: 'center',
+   },
+}))
+
+export default Header
