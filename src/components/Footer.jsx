6c61a1e21fbf58f97e045262824b6c9e850bd928
Aigerim Mamattoichu <aigerimma993@gmail.com>
2025-04-27 21:32:11 -0700

    budfix-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,199 +1,104 @@
-// import FacebookIcon from '@mui/icons-material/Facebook'
-// import InstagramIcon from '@mui/icons-material/Instagram'
-// import { styled } from '@mui/material/styles'
-// import { Box, Container, Typography } from '@mui/material';
-
-// const Footer = () => {
-//    return (
-
-//     <div>
-//       <StyledBox>
-//          <StyledContainer>
-//             <StyledText>GIFT LIST</StyledText>
-//             <StyledType>Навигация</StyledType>
-//             <StyledTitle>Социальная сеть нового поколения</StyledTitle>
-
-//             <StyledTitle>
-//                 О проекте
-//                  Благотворительность</StyledTitle>
-
-//             <StyledType>Подписаться на рассылку</StyledType>
-//             <FacebookIcon />
-//             <InstagramIcon />
-//          </StyledContainer>
-
-//       </StyledBox>
-
-// <StyledSoft>Peaksoft © 2022 Все права защищены</StyledSoft>
-// </div>
-//    )
-// }
-
-// export default Footer
-
-// const StyledBox = styled(Box)({
-//    width: '1440 px',
-//    top: '3128 px ',
-//    border: '1px solid #353A5A',
-//    opacsity: '10%',
-//    borderRadius: '1px ',
-// })
-
-// const StyledText = styled(Typography)({
-//    width: '113',
-//    height: '29',
-//    top: '3153px',
-//    left: '135px',
-
-//    fontFamily: 'Inter',
-//    fontWeight: '700',
-//    fontSize: '24px',
-//    lineHeight: '100%',
-//    letterSpacing: '0%',
-//    textTransform: 'uppercase',
-// })
-
-// const StyledContainer = styled(Container)({
-//    display: 'flex',
-//    justifyContent: 'space-around',
-//    marginTop: '100px',
-// })
-
-// const StyledTitle = styled(Typography)({
-//    width: '172',
-//    height: '16',
-//    top: '3219px',
-//    left: ' 634px',
-
-//    fontFamily: 'Inter',
-//    fontWeight: '400',
-//    fontSize: '16px',
-//    lineHeight: '100%',
-//    letterSpacing: '0%',
-// })
-
-// const StyledType = styled(Typography)({
-//    width: '96',
-//    height: '18',
-//    top: '3153px',
-//    left: '634px',
-
-//    fontFamily: 'Inter, san-serif',
-//    fontWeight: '500',
-//    fontSize: '18px',
-//    lineHeight: '100%',
-//    letterSpacing: '0%',
-//    paddingTop: '5px',
-// })
-
-// const StyledSoft = styled(Typography)({
-//    width: '265',
-//    height: '14',
-//    top: '3298px',
-//    left: '588px',
-
-//    fontFamily: 'Inter',
-//    fontWeight: '400',
-//    fontSize: '14px',
-//    lineHeight: '130%',
-//    letterSpacing: '0%',
-//    textAlign: 'center',
-//    color: '#020202',
-
-// })
-
-import {
-   Box,
-   Stack,
-   Container,
-   Grid,
-   Typography,
-   TextField,
-   Button,
-   styled,
-} from '@mui/material'
-
-// import {Inactive} from 'assets/icons/inactive.svg'
-
-// import { Facebook } from 'assets/icons/facebook.svg'
-// import { Instagram } from 'assets/icons/instagram.svg'
+import { styled } from '@mui/material/styles'
+import { Box, Container, Typography, TextField } from '@mui/material'
+import facebook from '../assets/icons/facebook.svg'
+import wk from '../assets/icons/wk.svg'
+import instagram from '../assets/icons/instagram.svg'
+import Inactive from '../assets/icons/inactive.svg'
 
 const Footer = () => {
    return (
-      <ContainerStack>
-         <Container>
-            <Grid container spacing={4}>
-               <Grid item xs={12} md={4}>
-                  <TypographyText>GIFT LIST</TypographyText>
-
+      <div>
+         <StyledBox>
+            <StyledContainer>
+               <div>
+                  <StyledText>GIFT LIST</StyledText>
                   <StyledTitle>Социальная сеть нового поколения</StyledTitle>
-
                   <Box>
-                     {/* <img src={Facebook} alt="" />
-                     <img src={Instagram} alt="" /> */}
+                     <img src={facebook} alt="" />
+                     <img src={wk} alt="" />
+                     <img src={instagram} alt="" />
                   </Box>
-               </Grid>
+               </div>
 
-               <Grid>
-                  <StyledText>Навигация</StyledText>
+               <div>
+                  <StyledType>Навигация</StyledType>
+                  <StyledTitle>О проекте Благотворительность</StyledTitle>
+               </div>
 
-                  <Text>О проекте</Text>
-                  <Text>Благотворительность</Text>
-               </Grid>
+               <div>
+                  <StyledType>Подписаться на рассылку</StyledType>
+                  <TextField
+                     variant="outlined"
+                     placeholder="Введите ваш Email"
+                     size="small"
+                  />
+                  <img src={Inactive} alt="" />
+               </div>
+            </StyledContainer>
+         </StyledBox>
 
-               <Grid>
-                  <StyledText>Подписаться на рассылку</StyledText>
-
-                  <StyledBox>
-                     <TextField
-                        variant="outlined"
-                        placeholder="Введите ваш Email"
-                        size="small"
-                     />
-                     <Button>➔</Button>
-                  </StyledBox>
-               </Grid>
-            </Grid>
-
-            <StyledSoft>Peaksoft © 2022 Все права защищены</StyledSoft>
-         </Container>
-      </ContainerStack>
+         <StyledSoft>Peaksoft © 2022 Все права защищены</StyledSoft>
+      </div>
    )
 }
 
 export default Footer
 
-const ContainerStack = styled(Stack)(() => ({
-   backgroundColor: '#f5f5f5',
-   margin: '5px',
-}))
+const StyledBox = styled(Box)({
+   width: '1440 px',
+   top: '3128 px ',
+   border: '1px solid #353A5A',
+   opacsity: '10%',
+   borderRadius: '1px ',
+})
 
-const StyledText = styled(Typography)(() => ({
+const StyledText = styled(Typography)({
+   width: '113',
+   height: '29',
+   top: '3153px',
+   left: '135px',
    fontFamily: 'Inter',
-   fontWeight: '500',
-   fontSize: '18px',
+   fontWeight: '700',
+   fontSize: '24px',
    lineHeight: '100%',
-}))
+   letterSpacing: '0%',
+   textTransform: 'uppercase',
+   color: '#020202',
+})
 
-const Text = styled(Typography)(() => ({
+const StyledContainer = styled(Container)({
+   display: 'flex',
+   justifyContent: 'space-evenly',
+   marginTop: '80px',
+   padding: '20px',
+})
+
+const StyledTitle = styled(Typography)({
+   width: '172',
+   height: '16',
+   top: '3219px',
+   left: ' 634px',
+
    fontFamily: 'Inter',
    fontWeight: '400',
    fontSize: '16px',
    lineHeight: '100%',
-}))
+   letterSpacing: '0%',
+})
 
-const StyledBox = styled(Box)(() => ({
-   border: '1px',
-   borderRadius: '50%',
-}))
+const StyledType = styled(Typography)({
+   width: '96',
+   height: '18',
+   top: '3153px',
+   left: '634px',
 
-const StyledTitle = styled(Typography)(() => ({
-   fontFamily: 'Inter',
-   fontWeight: '400',
-   fontSize: '14px',
-   lineHeight: '130%',
-   textAlign: 'center',
-}))
+   fontFamily: 'Inter, san-serif',
+   fontWeight: '500',
+   fontSize: '18px',
+   lineHeight: '100%',
+   letterSpacing: '0%',
+   paddingTop: '5px',
+})
 
 const StyledSoft = styled(Typography)({
    width: '265',
@@ -208,13 +113,4 @@ const StyledSoft = styled(Typography)({
    letterSpacing: '0%',
    textAlign: 'center',
    color: '#020202',
-   display: 'flex',
-   justifyContent: 'center',
-   alignItems: 'center',
 })
-
-const TypographyText = styled(Typography)(() => ({
-   fontFamily: 'Inter',
-   fontWeight: '700',
-   fontSize: '24px',
-}))
