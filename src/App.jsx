b1e2e222645a9d85963fb49895d69b801b441d5b
Aigerim Mamattoichu <aigerimma993@gmail.com>
2025-05-08 12:20:46 -0700

    bugfix-footer5

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-const App = () => <h1> Gift List JS-16</h1>
+import React from 'react'
+import Footer from './components/Footer'
+im
+
+const App = () => {
+   return (
+      <div>
+         <Footer />
+      </div>
+   )
+}
 
 export default App
