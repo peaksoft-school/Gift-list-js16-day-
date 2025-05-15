9e66cc95b70e7b604eae23e0dc1695292c193a22
Bekmyrza200800 <bekmyrza986@gmail.com>
2025-05-15 17:34:07 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-import toastifyNotify from './utils/helpers/ToastifyNotify'
-
-const App = () => (
-   <>
-      <button
-         onClick={() =>
-            toastifyNotify({
-               title: 'great',
-               message: 'asdasdf',
-               autoClose: 3000,
-               type: 'success',
-            })
-         }
-      >
-         Click
-      </button>
-      GIFT-LIST JS-16 DAY
-   </>
-)
+const App = () => <>GIFT-LIST JS-16 DAY</>
 
 export default App
