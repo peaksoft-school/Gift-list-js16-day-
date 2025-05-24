6c1ac1de4c1faf5d78a55b43ac393323c3e4fafc
Nargiza Naamatbekova <naamatbeknargiza@gmail.com>
2025-05-24 23:32:54 +0600

    corrected

--- src/configs/axiosInstance.js
+++ src/configs/axiosInstance.js
@@ -23,7 +23,7 @@ axiosInstance.interceptors.request.use(
       const { token } = customStore.getState()?.auth
 
       if (true) {
-         updateConfig.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NDc2MzMxNTQsImV4cCI6MTc0ODIzNzk1NH0.960y5ZICCfqpoVGfQsX0t1PxPjEp-Gzfm9v-tfMAO2M`
+         updateConfig.headers.Authorization = `Bearer ${token}`
       }
 
       return updateConfig
